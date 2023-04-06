e239a8ec1311e0caef558fb3bb30ec994217ab1b
Lisa <mkoliju@gmail.com>
2023-04-05 21:14:15 -0700

    Import game background image

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React, { FC } from 'react';
-import './App.css';
+
 
 
 
 const App: FC = () => {
   return (
-    <div className='text-3xl'>
-      Hello
+    <div className='text-3xl bg-background-image bg-cover h-screen'>
+      hello
     </div>
   );
 }
