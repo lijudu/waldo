2068ed2b4eb843740752c06dd282a6c3143ac85f
Lisa <mkoliju@gmail.com>
2023-04-13 09:19:14 -0700

    Image click produces character list at click spot

--- src/Components/GameImage.tsx
+++ src/Components/GameImage.tsx
@@ -1,18 +1,44 @@
-import * as React from 'react';
+import * as React from 'react'
+import { useState, useEffect } from 'react'
+import { Characters } from './Characters';
 
 export interface IAppProps {
+  x: number,
+  y: number,
 }
 
-export function GameImage (props: IAppProps) {
+
+export function GameImage () {
+
+  const [showCharacters, setShowCharacters] = useState(false)
+  const [target, setTarget] = useState([])
 
   const getLocation = (e: React.MouseEvent<HTMLInputElement>) => {
-    console.log(e.pageX, e.pageY)
-  }
-  
+
+    if (showCharacters === false) {
+      setShowCharacters(true)
+    } else if (showCharacters === true) {
+      setShowCharacters(false)
+    }
+
+    setTarget([e.pageX, e.pageY])
+
+    console.log(target)
+    }
+
+
   return (
     <div>
         <div className='bg-background-image bg-auto  h-[92rem] w-full bg-center bg-no-repeat'
               onClick={getLocation}/>
+        {showCharacters && (
+          <div className='z-10 absolute  text-3xl text-red-200' 
+            style={{top: `${target[1]}px`, left: `${target[0]}px`}}>
+            <Characters/>
+          </div>
+        )}
     </div>
   );
 }
+
+
